44197da871344548dd126bdabab182366100746c
Punith Reddy <punithnik8088@gmail.com>
2026-06-23 14:06:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    server: { entry: "server" },
+    server: { 
+      entry: "server",
+    },
   },
+  vite: {
+    base: '/punithreddy/', // <-- Tells the app to load assets from your subfolder
+  },
+  nitro: {
+    preset: 'github-pages', // <-- Optimizes the build output for GitHub Pages
+  }
 });
